1e8b98200c8ded43153c15a2622a1c56061593a1
jetwat <jetwatwarathip@gmail.com>
2026-04-01 15:04:11 +0700

    submitted exercise 8

--- exercises/exercise-08.js
+++ exercises/exercise-08.js
@@ -5,14 +5,17 @@
 // Hint: you only need one line to replace the entire if...else block.
 
 const cartTotal = 1200;
-let shippingLabel;
+let shippingLabel = cartTotal >= 1000 ? "FREE" : "$9.99";
 
-if (cartTotal >= 1000) {
-  shippingLabel = "FREE";
-} else {
-  shippingLabel = "$9.99";
-}
+// if (cartTotal >= 1000) {
+//   shippingLabel = "FREE";
+// } else {
+//   shippingLabel = "$9.99";
+// }
 
 console.log("Shipping: " + shippingLabel);
 
 // Refactor:
+// -> โจทย์อยากให้ refactor โดยใช้ ternary เท่านั้น
+// -> แก้โดยก้อปคอนดิชันและผลต่าง ๆ ไปวาง ในบรรทัด 12
+// -> ลองรัน -- ok
