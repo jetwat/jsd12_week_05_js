ba29b08b82cff6db6127344ffd6f13a702875cf1
jetwat <jetwatwarathip@gmail.com>
2026-04-01 15:17:51 +0700

    resubmitted exercise 7

--- exercises/exercise-07.js
+++ exercises/exercise-07.js
@@ -7,7 +7,7 @@
 //   anything else → "Status unknown. Please contact support."
 // When orderStatus is "cancelled", nothing is logged. Fix the code.
 
-const orderStatus = "ป";
+const orderStatus = "cancelled";
 
 if (orderStatus === "processing") {
   console.log("Your order is being processed.");
@@ -15,8 +15,8 @@ if (orderStatus === "processing") {
   console.log("Your order is on the way!");
 } else if (orderStatus === "delivered") {
   console.log("Your order has been delivered.");
-} else if (orderStatus === "cancelled") {
-  console.log("");
+} else {
+  console.log("Status unknown. Please contact support.");
 }
 
 // Fix:
@@ -24,4 +24,6 @@ if (orderStatus === "processing") {
 // -> เท่าที่เห็น น่าจะไม่มีค่าออกมาเพราะไม่มีการเขียน `else` ไว้
 //    ทำให้ ไม่สามารถส่งค่า "Status unknown. Please contact support." ออกมาได้
 //    อีกทั้งยังไม่มีการใส่เงื่อนไขหากเป็น "cancelled" ซึ่งหากไม่มีเงื่อนไข anything else ก็คงพอถูไถไปได้
-// -> เพิ่ม else if cancelled ลองรัน -- ok
+// -> เพิ่ม else if cancelled ลองรัน -- ลืมใส่ else "Status unknown. Please contact support."
+// -> กลับมาอ่านโจทย์ใหม่ เข้าใจผิด นึกว่าอยากให้ไม่ส่งค่าหาก เงื่อนไข `= "cancelled"`
+// -> แก้แล้วรันใหม่
