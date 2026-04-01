dc7f8222c13271b393f57711737773d4754bafcd
jetwat <jetwatwarathip@gmail.com>
2026-04-01 14:40:14 +0700

    submitted exercise 5

--- exercises/exercise-05.js
+++ exercises/exercise-05.js
@@ -8,7 +8,11 @@ const isOnSale = true;
 const salePrice = 29.99;
 const originalPrice = 49.99;
 
-const displayPrice = isOnSale ? salePrice originalPrice;
+const displayPrice = isOnSale ? salePrice : originalPrice;
 console.log("Price: $" + displayPrice);
 
 // Fix:
+// -> เปิดมาโค้ดก็แดงเลย extension ช่วย ไม่ต้องลองรัน 555
+// -> colon หายไป จาก ternary operator ลองใส่แล้วหายแดง
+// -> ลองรัน ผล "Price: $29.99" ออกหน้าคอนโซล
+// -> กลับไปอ่านโจทย์ สรุปว่าโอเคแล้ว
